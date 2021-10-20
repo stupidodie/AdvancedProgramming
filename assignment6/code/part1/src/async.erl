5d0764c1474368158bdb085280d1dd8976278b81
GuanRan Tai <taiguanran@gmail.com>
2021-10-20 14:21:22 +0200

    Update async.erl

--- assignment6/code/part1/src/async.erl
+++ assignment6/code/part1/src/async.erl
@@ -1,44 +1,92 @@
 -module(async).
--behaviour(gen_statem).
+
+-behaviour(gen_server).
+
 -export([new/2, wait/1, poll/1, wait_catch/1, wait_any/1]).
--export([callback_mode/0,init/1]).
--export([calculate/3,handle_common/3]).
-new(Fun, Arg) -> 
-    gen_statem:start({local,?MODULE}, ?MODULE, [{Fun,Arg}], []).
-wait(Aid) -> 
-    case gen_statem:call(Aid, ask) of
-        nothing->wait(Aid);
-        {ok,Res}->Res;
-        {exception, Ex}->throw(Ex)
-    end.
-poll(Aid) -> gen_statem:call(Aid, ask).
-wait_catch(Aid) -> 
-    case gen_statem:call(Aid, ask) of
-        nothing -> wait_catch(Aid);
-        {ok, Res} -> {ok, Res};
-        {exception, Ex} -> {exception, Ex}
-    end.
-wait_any(Aids) -> nope.
+-export([init/1]).
+-export([handle_cast/2, handle_call/3]).
+-export([terminate/2]).
 
--define(HANDLE_COMMON,
-    ?FUNCTION_NAME(T, C, D) -> handle_common(T, C, D)).
-handle_common({call,From},ask,Data) ->
-    {keep_state, Data,
-     [{reply,From,Data}]}.
-calculate(cast,{exception,Reason},_) ->
-    {keep_state,{exception,Reason}};
-calculate(cast,{ok,Result},_) ->
-    {keep_state,{ok,Result}};
-    ?HANDLE_COMMON.
+new(Fun, Arg) ->
+    case gen_server:start(?MODULE, {Fun, Arg}, []) of
+        {ok, Aid} ->
+            Aid
+    end.
 
-init({Fun,Arg}) ->
-    spawn(fun()->
-        try
-            Result=Fun(Arg),
-            gen_statem:cast(?MODULE, {ok,Result})
-        catch
-            _:Reason->gen_statem:cast(?MODULE, {exception,Reason})
+wait(Aid) ->
+    case gen_server:call(Aid, wait) of
+        {ok, Res} ->
+            Res;
+        {exception, Ex} ->
+            throw(Ex)
+    end.
+
+poll(Aid) ->
+    gen_server:call(Aid, poll).
+
+wait_catch(Aid) ->
+    gen_server:call(Aid, wait).
+
+wait_any(Aids) ->
+    Work =
+        fun(X) ->
+           Worker = self(),
+           lists:foreach(fun(Aid) ->
+                            spawn(fun() ->
+                                     Result = gen_server:call(Aid, wait),
+                                     Worker ! {Aid, Result}
+                                  end)
+                         end,
+                         X),
+        receive 
+           {Aid, Res} -> {Aid, Res} 
         end
-    end),
-    {ok,calculate,nothing}.
-callback_mode()->state_function.
+    end,
+    Async = new(Work, Aids),
+    case wait(Async) of
+        {Aid,{ok,Result}}->{Aid,Result};
+        {_,{exception,Reason}}->throw(Reason)
+    end.
+
+init({Fun, Arg}) ->
+    Me = self(),
+    spawn(fun() ->
+             try
+                 Result = Fun(Arg),
+                 gen_server:cast(Me, {ok, Result})
+             catch
+                 Reason -> gen_server:cast(Me, {exception, Reason})
+             end
+          end),
+    {ok, []}.
+
+handle_call(poll, _From, State) ->
+    case State of
+        {ok, _} ->
+            {reply, State, State};
+        {exception, _} ->
+            {reply, State, State};
+        _ ->
+            {reply, nothing, State}
+    end;
+handle_call(wait, From, State) ->
+    case State of
+        {ok, _} ->
+            {reply, State, State};
+        {exception, _} ->
+            {reply, State, State};
+        _ ->
+            {noreply, [From | State]}
+    end.
+
+handle_cast(NewState, State) ->
+    case State of
+        [] ->
+            {noreply, NewState};
+        _ ->
+            lists:foreach(fun(From) -> gen_server:reply(From, NewState) end, State),
+            {noreply, NewState}
+    end.
+
+terminate(_Reason, _State) ->
+    ok.
